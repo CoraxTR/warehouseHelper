e5b2d0636ef4bf7143b40701eb7370997fec2ca6
boris <coraxtr@mail.ru>
2026-03-18 14:12:25 +0300

    just checking

--- go.mod
+++ go.mod
@@ -9,16 +9,25 @@ require (
 )
 
 require (
+	github.com/clipperhouse/uax29/v2 v2.2.0 // indirect
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/pkcs7 v0.2.0 // indirect
+	github.com/hhrutter/tiff v1.0.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/pdfcpu/pdfcpu v0.11.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/richardlehane/mscfb v1.0.6 // indirect
 	github.com/richardlehane/msoleps v1.0.6 // indirect
 	github.com/tiendc/go-deepcopy v1.7.2 // indirect
 	github.com/xuri/efp v0.0.1 // indirect
 	github.com/xuri/nfp v0.0.2-0.20250530014748-2ddeb826f9a9 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
+	golang.org/x/image v0.32.0 // indirect
 	golang.org/x/net v0.50.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ internal/exporter/pdf/exporter.go
@@ -0,0 +1,65 @@
+package pdf
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
+)
+
+type PDFExporter struct{}
+
+func NewPDFExporter() *PDFExporter {
+	return &PDFExporter{}
+}
+
+func (e *PDFExporter) ExportOrderPDF(data []byte) (string, error) {
+	conf := model.NewDefaultConfiguration()
+	validated, err := api.ReadAndValidate(bytes.NewReader(data), conf)
+	if err != nil {
+		log.Printf("couldn't validate, %v", err)
+	}
+	outFile, err := os.Create("exported.pdf")
+	if err != nil {
+		return "", err
+	}
+	defer outFile.Close()
+
+	err = api.Write(validated, outFile, conf)
+	if err != nil {
+		return "", err
+	}
+	// На данный момент сохраняем в cmd, позже переделаем в отдельную папку
+	return outFile.Name(), nil
+}
+
+func (e *PDFExporter) ExportMergedPDF(data [][]byte) (string, error) {
+	conf := model.NewDefaultConfiguration()
+
+	readers := make([]io.ReadSeeker, len(data))
+	for i, b := range data {
+		readers[i] = bytes.NewReader(b)
+	}
+
+	timestamp := time.Now().Format("20060102_150405")
+	filename := fmt.Sprintf("merged_%s.pdf", timestamp)
+	fullpath := "../" + filename
+
+	file, err := os.Create(fullpath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	err = api.MergeRaw(readers, file, false, conf)
+	if err != nil {
+		return "", err
+	}
+
+	return fullpath, nil
+}
